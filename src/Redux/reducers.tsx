83ab3efd782e50ee47bd4c06dd66bdfd128fb429
Nazar <nazar.pavlenko13@gmail.com>
2023-11-15 18:10:40 +0100

    task(Redux): create new files and setup redux

---
+++ src/Redux/reducers.tsx
@@ -0,0 +1,9 @@
+interface AppState {}
+const initialState: AppState = {};
+const appReducer = (state = initialState, action: any) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+export default appReducer;
