9476032493f9529d960fb88097e9e41e29287b41
Nazar <nazar.pavlenko13@gmail.com>
2023-11-15 23:16:30 +0100

    task(todoItem): create todo item

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        itemBG: '#5b21b6',
+        itemLine: '#6c28d8',
+        itemText: '#c5b4f1',
+        itemTitle: '#e2e9f1',
+      }
+    },
   },
   plugins: [],
 }
