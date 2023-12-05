3b0d3e08a1ea41550ca01ec3d8c028fe047cb223
Nazar <nazar.pavlenko13@gmail.com>
2023-12-05 22:21:10 +0100

    task(components): create Item component

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,13 @@ module.exports = {
         text: {
           primary: '#f8ecea',
           light: '#94a2b9',
-          disabled: '#000000'
+          disabled: '#000000',
+          defaultTitle: '#d4e9f0',
+          default: '#3a668a',
         },
         primaryBG: '#5b20b7',
         disabledBG: '#808080',
+        defaultBG: '#141f32',
       }
     },
   },
