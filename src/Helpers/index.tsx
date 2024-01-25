9a46c1dd3965eda11989115d665662c953ac9588
Nazar <nazar.pavlenko13@gmail.com>
2024-01-25 21:56:22 +0100

    fix(components): create new func in Helpers and fix Header component

--- src/Helpers/index.tsx
+++ src/Helpers/index.tsx
@@ -1,3 +1,11 @@
 import React from "react";
 
 export const mock = () => {};
+
+export const getFormattedDate = (date = new Date()) => {
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const month = currentDate.toLocaleString("en", { month: "short" });
+  const day = currentDate.getDate();
+  return `${year}, ${month} ${day}`;
+};
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,24 +1,20 @@
-import React from "react";
+import React, { ReactNode } from "react";
+
+import { getFormattedDate } from "../../Helpers";
+import { mock } from "../../Helpers";
 
 import Button from "../Button";
-import { mock } from "../../Helpers";
 
 type HeaderProps = {
   onAdd: () => void;
+  date: string;
 };
 
-const Header = ({ onAdd = mock }: HeaderProps) => {
-  const currentDate = new Date();
-  const year = currentDate.getFullYear();
-  const month = currentDate.toLocaleString("eng", { month: "short" });
-  const day = currentDate.getDate();
-
+const Header = ({ onAdd = mock, date = getFormattedDate() }: HeaderProps) => {
   return (
-    <div className="flex relative justify-between items-center w-[100%] min-h-[80px] bg-modalBG">
-      <div className="text-text-light py-2 px-4 m-auto">
-        {`${year}, ${month} ${day}`}
-      </div>
-      <div className="absolute right-[30px] top-[20px]">
+    <div className="flex relative justify-between items-center w-[100%] min-h-[80px] p-[5px] bg-modalBG">
+      <div className="text-text-light py-2 px-4 m-auto">{date}</div>
+      <div className="absolute right-[20px] top-[20px]">
         <Button onClick={onAdd} text={"Add new Task"} />
       </div>
     </div>
