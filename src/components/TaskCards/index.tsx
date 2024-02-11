84543b3a5d19b12915005d51c1e768452eedef34
Nazar <nazar.pavlenko13@gmail.com>
2024-02-11 11:00:20 +0100

    fix(TaskCards): fix component

--- src/components/TaskCards/index.tsx
+++ src/components/TaskCards/index.tsx
@@ -9,22 +9,20 @@ type TaskCardsProps = {
   tasks: Task[];
 };
 
-const TaskCards = ({ tasks }: TaskCardsProps) => {
-  return (
-    <div className="grid grid-cols-5 gap-4 p-4 h-full">
-      {tasks &&
-        tasks.map((card: Task, index: number) => (
-          <div key={index}>
-            <Card
-              title={card.title}
-              text={card.description}
-              type={card.type}
-              onDelete={mock}
-            />
-          </div>
-        ))}
-    </div>
-  );
-};
+const TaskCards = ({ tasks }: TaskCardsProps) => (
+  <div className="grid grid-cols-5 gap-4 p-4 h-full">
+    {tasks &&
+      tasks.map((card: Task, index: number) => (
+        <div key={index}>
+          <Card
+            title={card.title}
+            text={card.description}
+            type={card.type}
+            onDelete={mock}
+          />
+        </div>
+      ))}
+  </div>
+);
 
 export default TaskCards;
