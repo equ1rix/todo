1ca1e31c10c5b83ae0ddd6b2cd0210e8f6e8f646
equ1rix <103569669+equ1rix@users.noreply.github.com>
2024-03-20 19:11:49 +0100

    task(src): add mocked tasks

--- src/Helpers/mock.tsx
+++ src/Helpers/mock.tsx
@@ -1,8 +1,9 @@
 export const defaultTasks = [
   {
     id: "0",
-    title: "First task",
-    description: "test text #1",
+    title: "Conduct Weekly Progress Review",
+    description:
+      "Hold a weekly meeting to review the progress of ongoing projects, address any challenges, and adjust plans as necessary.",
     isFavorite: false,
     type: "DEFAULT",
     dueDate: new Date().toISOString(),
@@ -10,20 +11,102 @@ export const defaultTasks = [
   },
   {
     id: "1",
-    title: "Second task",
-    description: "test text #2",
+    title: "Perform Daily Backups",
+    description:
+      "Ensure that important data and files are regularly backed up to prevent loss in case of system failure or data corruption.",
     isFavorite: false,
+    type: "DEFAULT",
+    dueDate: "2024-06-01T12:00:00.000Z",
+    createdAt: new Date().toISOString(),
+  },
+  {
+    id: "2",
+    title: "Update To-Do List",
+    description:
+      "Take a few minutes at the beginning or end of each day to update your to-do list with any new tasks, prioritize them, and mark off completed items.",
+    isFavorite: true,
+    type: "DEFAULT",
+    dueDate: "2024-04-17T12:00:00.000Z",
+    createdAt: new Date().toISOString(),
+  },
+  {
+    id: "3",
+    title: "Write Daily Report",
+    description:
+      "Compile and submit a daily report summarizing tasks completed, progress made, and any obstacles encountered.",
+    isFavorite: false,
+    type: "DEFAULT",
+    dueDate: "2024-05-22T12:00:00.000Z",
+    createdAt: new Date().toISOString(),
+  },
+  {
+    id: "4",
+    title: "Respond to Emails",
+    description:
+      "Check and respond to emails from clients, colleagues, and stakeholders in a timely manner.",
+    isFavorite: true,
     type: "DEFAULT",
     dueDate: new Date().toISOString(),
     createdAt: new Date().toISOString(),
   },
   {
-    id: "2",
-    title: "Third task",
-    description: "test text #3",
+    id: "5",
+    title: "Attend Daily Stand-up Meeting",
+    description:
+      "Participate in a brief daily stand-up meeting with the team to discuss progress, goals, and potential blockers.",
     isFavorite: true,
     type: "DEFAULT",
-    dueDate: new Date().toISOString(),
+    dueDate: "2024-03-24T12:00:00.000Z",
+    createdAt: new Date().toISOString(),
+  },
+  {
+    id: "6",
+    title: "Review Code Changes",
+    description:
+      "Review and provide feedback on code changes submitted by team members through pull requests or code reviews.",
+    isFavorite: false,
+    type: "DEFAULT",
+    dueDate: "2024-04-15T12:00:00.000Z",
+    createdAt: new Date().toISOString(),
+  },
+  {
+    id: "7",
+    title: "Conduct User Testing",
+    description:
+      "Perform user testing on new features or updates to gather feedback and identify any usability issues.",
+    isFavorite: false,
+    type: "DEFAULT",
+    dueDate: "2024-06-02T12:30:00.000Z",
+    createdAt: new Date().toISOString(),
+  },
+  {
+    id: "8",
+    title: "Update Project Documentation",
+    description:
+      "Keep project documentation up-to-date, including requirements, design documents, and user manuals.",
+    isFavorite: false,
+    type: "DEFAULT",
+    dueDate: "2024-05-13T12:00:00.000Z",
+    createdAt: new Date().toISOString(),
+  },
+  {
+    id: "9",
+    title: "Plan Next Day's Tasks",
+    description:
+      "Review the upcoming tasks and priorities, and plan the tasks for the next day to ensure smooth workflow and productivity.",
+    isFavorite: true,
+    type: "DEFAULT",
+    dueDate: "2024-03-26T12:00:00.000Z",
+    createdAt: new Date().toISOString(),
+  },
+  {
+    id: "10",
+    title: "Daily Prioritization",
+    description:
+      "Prioritize tasks every morning to focus on the most important and urgent ones first, ensuring productivity and efficiency throughout the day.",
+    isFavorite: false,
+    type: "DEFAULT",
+    dueDate: "2024-03-29T12:00:00.000Z",
     createdAt: new Date().toISOString(),
   },
 ];
